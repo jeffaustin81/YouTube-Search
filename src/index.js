d6b492cb41b00ebe5a7beb7acf667f67de0187e0
Jeff Austin <jeffaustin81@gmail.com>
2016-03-01 14:33:36 -0800

    Initial commit, App component, SearchBar component

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyBVPFPTjLR-Y3FM239qlvF7-IPMZE8Dpx0';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
